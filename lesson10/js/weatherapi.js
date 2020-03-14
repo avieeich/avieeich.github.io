36e444b7969ff56b5c93b81ab95ca45299c47993
avieeich <avie.eich@gmail.com>
2020-03-14 17:06:01 -0600

    Preston Weather API upload

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,15 @@
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=eb2afd8bdd68cf1db8f496b787a6d403'
+
+fetch(apiURL)
+.then((response) => response.json())
+.then((jsObject) => {
+    console.log(jsObject);
+    const currentTemp=document.querySelector('#current-temp');
+    
+    currentTemp.textContent = jsObject.main.temp;
+    const imagesrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute ('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);
+});
