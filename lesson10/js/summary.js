5072d2342fbb12777b47ddff6bcc2676bb0211e6
avieeich <avie.eich@gmail.com>
2020-03-18 22:25:33 -0600

    https change

--- lesson10/js/summary.js
+++ lesson10/js/summary.js
@@ -1,4 +1,4 @@
-const apiURLS = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=eb2afd8bdd68cf1db8f496b787a6d403'
+const apiURLS = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=eb2afd8bdd68cf1db8f496b787a6d403'
 
 fetch(apiURLS)
 .then((response) => response.json())
